41449feadbc16f56e799e0a147ea43a2797c834e
odonyde <odonyde@gmail.com>
2021-04-29 20:05:02 -0300

    Add load tests

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -59,7 +59,7 @@ namespace UITests
                 // Navigate to the site.
                 // The site name is stored in the SITE_URL environment variable to make 
                 // the tests more flexible.
-                string url = Environment.GetEnvironmentVariable("uiTestsSiteUrl");
+                string url = Environment.GetEnvironmentVariable("webAppUrlTest");
                 driver.Navigate().GoToUrl(url + "/");
 
                 // Wait for the page to be completely loaded.
